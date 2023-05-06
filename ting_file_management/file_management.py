bded8f87000373d2f6a70903f3f4ac8c9f6dcb89
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2023-05-06 11:03:22 -0300

    feat: text_importer implemented

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,11 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+
+def txt_importer(path_file: str):
+    try:
+        if not path_file.__contains__(".txt"):
+            print("Formato inválido", file=sys.stderr)
+        with open(path_file, "r") as file:
+            return list(file.read().splitlines())
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)

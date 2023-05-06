386b9aa23e984af0eaeeac2cf36d984c95589b84
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2023-05-06 14:00:03 -0300

    feat: word_search functions implemented

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,33 @@
+from ting_file_management.file_management import txt_importer
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    text = txt_importer(instance.queue[0])
+    if not any(word.lower() in line.lower() for line in text):
+        return []
+    return [
+        {
+            "palavra": word,
+            "arquivo": instance.queue[0],
+            "ocorrencias": [{"linha": text.index(d) + 1} for d in text
+                            if word.lower() in d.lower()]
+        }
+    ]
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    text = txt_importer(instance.queue[0])
+    if not any(word.lower() in line.lower() for line in text):
+        return []
+    return [
+        {
+            "palavra": word,
+            "arquivo": instance.queue[0],
+            "ocorrencias": [{
+                "linha": text.index(d) + 1,
+                "conteudo": d
+            }
+                            for d in text
+                            if word.lower() in d.lower()]
+        }
+    ]

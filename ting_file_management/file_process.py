cb305f6e8512463a45e8107d924470509fc72a8b
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2023-05-06 11:48:57 -0300

    feat: file_process functions implemented

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,39 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if path_file not in instance.queue:
+        text_imp = txt_importer(path_file)
+        instance.enqueue(path_file)
+        out_dict = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(text_imp),
+            "linhas_do_arquivo": text_imp
+        }
+        print(out_dict, file=sys.stdout)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    try:
+        path_file = instance.queue[0]
+        if len(instance.queue) == 0:
+            print("Não há elementos", file=sys.stdout)
+        instance.dequeue()
+        print(f"Arquivo {path_file} removido com sucesso", file=sys.stdout)
+    except IndexError:
+        print("Não há elementos", file=sys.stdout)
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        path_file = instance.queue[position]
+        text_imp = txt_importer(path_file)
+        out_dict = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(text_imp),
+            "linhas_do_arquivo": text_imp
+        }
+        print(out_dict, file=sys.stdout)
+    except IndexError:
+        print("Posição inválida", file=sys.stderr)
